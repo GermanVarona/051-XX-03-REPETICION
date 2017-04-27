cfc0fc8b0ad5d1cb59d3c1f2cbf5240d73b9551e
GermanVarona <varona.german@yahoo.com>
2017-04-26 23:53:32 -0300

    Add files via upload

---
+++ Iteración.cpp
@@ -0,0 +1,23 @@
+/* TP#3 Repetición de frase
+Interación
+German O Varona
+K1051
+20170404
+*/
+
+#include "stdafx.h"
+#include <iostream>
+#include<conio.h>
+
+int main2()
+{
+	int	a;
+	a = 1;
+	while (a <= 10)
+	{
+		std::cout << "Siempre que (a) sea menor o igual a 10 aparezco\n";
+		a = a + 1;
+	};
+	getch();
+	return 0;
+}
---
+++ Saltos.cpp
@@ -0,0 +1,20 @@
+/* TP#3 Repetición de frase
+Saltos
+German O Varona
+K1051
+20170426
+*/
+
+#include "stdafx.h"
+#include <iostream>
+#include<conio.h>
+
+int main()
+{
+	for (int a = 0; a < 10; a++)
+	{
+		std::cout << "Hasta que (a) no sea igual a 10 no dejo de aparecer\n";
+	};
+	getch();
+	return 0;
+}
